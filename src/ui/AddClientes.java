9914ccc68fdc49da4a9fa043fbd977f1c73323d0
Brandow Buenos <brandowbuenos@Brandows-Macbook-Pro.local>
2020-01-28 21:04:47 -0300

    feat: fix bugs and improvements in UI

--- src/ui/AddClientes.java
+++ src/ui/AddClientes.java
@@ -14,27 +14,29 @@ public class AddClientes extends JPanel implements ActionListener {
     private JLabel customerIcon;
 
     private JButton bVoltar;
+    private final int WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    private final int HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 
     public AddClientes() {
         body = new JPanel();
-        body.setBounds(65, 330, 1790,540);
+        body.setBounds( WIDTH/30 , WIDTH/6, WIDTH - WIDTH/15,WIDTH/ 4);
         body.setBackground(new Color(255, 255, 255));
         body.setLayout(null);
         Janela.frame.getContentPane().add(body);
 
         description = new JPanel();
-        description.setBounds(0, 0, 246, 530);
+        description.setBounds(0, 0, WIDTH/8, WIDTH/4);
         description.setBackground(new Color(234, 234, 234));
         body.add(description);
 
         customerIcon = new JLabel();
         customerIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/customer-add.png")));
-        customerIcon.setBounds(50, 50, 0, 0);
+        customerIcon.setBounds(WIDTH/38, HEIGHT/2, 0, 0);
         description.add(customerIcon);
 
         JButton bFoto = new JButton();
         bFoto.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/picture-customer.png")));
-        bFoto.setBounds(770, 40, 120, 120);
+        bFoto.setBounds(WIDTH/2, WIDTH/48, WIDTH/17, WIDTH/17);
         bFoto.setContentAreaFilled(false);
         bFoto.setBorderPainted(false);
         bFoto.setFocusable(false);
@@ -42,50 +44,51 @@ public class AddClientes extends JPanel implements ActionListener {
         body.add(bFoto);
 
         JLabel nome = new JLabel("Nome");
-        nome.setBounds(600, 130, 200, 60);
+        nome.setBounds(WIDTH/3 + WIDTH/13, WIDTH/15, WIDTH/10, WIDTH/32);
         nome.setFont(new Font("Helvetica Neue", Font.PLAIN, 25));
         nome.setForeground(new Color(131, 131, 131));
         body.add(nome);
 
         JTextField tNome = new JTextField("");
-        tNome.setBounds(600, 200, 448, 60);
+        tNome.setBounds(WIDTH/3 + WIDTH/13, WIDTH/10, WIDTH/4, WIDTH/32);
         tNome.setFont(new Font("Helvetica Neue", Font.PLAIN, 23));
         tNome.setForeground(new Color(47, 47, 47));
         body.add(tNome);
 
         JLabel contatos = new JLabel("Contatos");
-        contatos.setBounds(600, 270, 200, 60);
+        contatos.setBounds(WIDTH/3+ WIDTH/13, WIDTH/8, WIDTH/10, WIDTH/32);
         contatos.setFont(new Font("Helvetica Neue", Font.PLAIN, 25));
         contatos.setForeground(new Color(131, 131, 131));
         body.add(contatos);
 
         JTextField tDDDContato1 = new JTextField("");
-        tDDDContato1.setBounds(600, 350, 70, 60);
+        tDDDContato1.setBounds(WIDTH/3+ WIDTH/13, WIDTH/6 - WIDTH /85, WIDTH/27, WIDTH/32);
         tDDDContato1.setFont(new Font("Helvetica Neue", Font.PLAIN, 23));
         tDDDContato1.setForeground(new Color(47, 47, 47));
         body.add(tDDDContato1);
 
         JTextField tContato1 = new JTextField("");
-        tContato1.setBounds(687, 350, 362, 60);
+        tContato1.setBounds(WIDTH/3+ WIDTH/8, WIDTH/6 - WIDTH /85, WIDTH/5, WIDTH/32);
         tContato1.setFont(new Font("Helvetica Neue", Font.PLAIN, 23));
         tContato1.setForeground(new Color(47, 47, 47));
         body.add(tContato1);
 
         JTextField tDDDContato2 = new JTextField("");
-        tDDDContato2.setBounds(600, 430, 70, 60);
+        tDDDContato2.setBounds(WIDTH/3+ WIDTH/13, WIDTH/5 - WIDTH /85, WIDTH/27, WIDTH/32);
         tDDDContato2.setFont(new Font("Helvetica Neue", Font.PLAIN, 23));
         tDDDContato2.setForeground(new Color(47, 47, 47));
         body.add(tDDDContato2);
 
         JTextField tContato2 = new JTextField("");
-        tContato2.setBounds(687, 430, 362, 60);
+        tContato2.setBounds(WIDTH/3+ WIDTH/8, WIDTH/5- WIDTH /85, WIDTH/5, WIDTH/32);
         tContato2.setFont(new Font("Helvetica Neue", Font.PLAIN, 23));
         tContato2.setForeground(new Color(47, 47, 47));
         body.add(tContato2);
 
         JButton bCancelar = new JButton();
         bCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/cancel-button.png")));
-        bCancelar.setBounds(1520, 420, 80, 80);
+        bCancelar.setBounds(WIDTH - WIDTH/4, WIDTH/5, 70, 70);
+        // Width/24 e width/24 anteriormente
         bCancelar.setContentAreaFilled(false);
         bCancelar.setBorderPainted(false);
         bCancelar.setFocusable(false);
@@ -94,7 +97,8 @@ public class AddClientes extends JPanel implements ActionListener {
 
         JButton bConfirmar = new JButton();
         bConfirmar.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/confirm-button.png")));
-        bConfirmar.setBounds(1600, 420, 160, 80);
+        bConfirmar.setBounds(WIDTH - WIDTH/5, WIDTH/5, 150, 70);
+        // Width/12 e width/24 anteriormente
         bConfirmar.setContentAreaFilled(false);
         bConfirmar.setBorderPainted(false);
         bConfirmar.setFocusable(false);
@@ -102,7 +106,7 @@ public class AddClientes extends JPanel implements ActionListener {
         body.add(bConfirmar);
 
         bVoltar = new JButton("<");
-		bVoltar.setBounds(300, 20, 40, 40);
+		bVoltar.setBounds(WIDTH/6, WIDTH/96, WIDTH/45, WIDTH/45);
 		bVoltar.setFont(new Font("Arial", Font.PLAIN, 20));
 		bVoltar.setForeground(new Color(205, 92, 92));
 		bVoltar.addActionListener(this);

35c9484371bdd992c822c0c1a5b03fe69b447019
Brandow Buenos <brandowbuenosaires@gmail.com>
2020-02-12 16:54:40 -0300

    feat: fix Janela for other resolutions

--- src/ui/Janela.java
+++ src/ui/Janela.java
@@ -38,23 +38,20 @@ public class Janela implements ActionListener {
         Dimension d = tk.getScreenSize();
         width = (int) d.getWidth();
         height = (int) d.getHeight();
-
+        
         // Frame
 
         frame = new JFrame("Barbearia Navalhas");
         frame.getContentPane().setBackground(new Color(247, 247, 247));
-        frame.setSize(width, height);
+        frame.setSize(1366, 768);
         frame.setLayout(null);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setResizable(false);
 
         // Painel de conteudo (nesta posicao para nao atrapalhar na transparencia)
 
         panel = new JPanel();
-        panel.setBounds(65, 400, 1790, 350);
+        panel.setBounds(45, 182, 1275, 470);
         panel.setBackground(new Color(255, 255, 255));
         panel.setLayout(null);
         frame.getContentPane().add(panel);
@@ -62,36 +59,36 @@ public class Janela implements ActionListener {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy -- hh:mm");
 
         message = new JLabel("Olá :) , hoje é: " + dateFormat.format(new Date()));
-        message.setBounds(700, 150, 420, 25);
-        message.setFont(new Font("Helvetica Neue", Font.PLAIN, 23));
+        message.setBounds(480, 230, 420, 25);
+        message.setFont(new Font("Helvetica Neue", Font.PLAIN, 21));
         panel.add(message);
 
         // Fundo
 
         fundo = new JLabel();
         fundo.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/background.png")));
-        fundo.setBounds(0, 0, width, height);
+        fundo.setBounds(0, 0, 1366, 768);
         frame.getContentPane().add(fundo);
 
         // Logo
 
         logo = new JLabel("");
         logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/logo.png")));
-        logo.setBounds(height / 17, height / 43, height / 3, height / 4);
+        logo.setBounds(49, 5, 160, 160);
         frame.getContentPane().add(logo);
 
         // Menu
 
         menu = new JPanel();
-        menu.setBounds(width / 5 + 30, width / 19 + 20, width / 2 + 380, width / 24);
+        menu.setBounds(276, 55, 1045, 56);
         menu.setBackground(new Color(34, 34, 34));
         menu.setLayout(null);
 
         bClientes = new JButton("    Clientes");
         bClientes.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/customer-icon.png")));
-        bClientes.setFont(new Font("Helvetica Neue", Font.PLAIN, 25));
+        bClientes.setFont(new Font("Helvetica Neue", Font.PLAIN, 19));
         bClientes.setForeground(new Color(255, 255, 255));
-        bClientes.setBounds(-40, -10, 488, 80);
+        bClientes.setBounds(0, 0, 348, 56);
         bClientes.setContentAreaFilled(false);
         bClientes.setBorderPainted(false);
         bClientes.setFocusable(false);
@@ -100,9 +97,9 @@ public class Janela implements ActionListener {
 
         bAssinaturas = new JButton("    Assinaturas");
         bAssinaturas.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/signature-icon.png")));
-        bAssinaturas.setFont(new Font("Helvetica Neue", Font.PLAIN, 25));
+        bAssinaturas.setFont(new Font("Helvetica Neue", Font.PLAIN, 19));
         bAssinaturas.setForeground(new Color(255, 255, 255));
-        bAssinaturas.setBounds(width / 4 - 40, -10, 488, 80);
+        bAssinaturas.setBounds(348,0,348,56);
         bAssinaturas.setContentAreaFilled(false);
         bAssinaturas.setBorderPainted(false);
         bAssinaturas.setFocusable(false);
@@ -111,9 +108,9 @@ public class Janela implements ActionListener {
 
         bRelatorios = new JButton("    Relatórios");
         bRelatorios.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/report-icon.png")));
-        bRelatorios.setFont(new Font("Helvetica Neue", Font.PLAIN, 25));
+        bRelatorios.setFont(new Font("Helvetica Neue", Font.PLAIN, 19));
         bRelatorios.setForeground(new Color(255, 255, 255));
-        bRelatorios.setBounds(width / 2 - 40, -10, 493, 80);
+        bRelatorios.setBounds(696,0,348,56);
         bRelatorios.setContentAreaFilled(false);
         bRelatorios.setBorderPainted(false);
         bRelatorios.setFocusable(false);
@@ -125,37 +122,37 @@ public class Janela implements ActionListener {
 
         whatsapp = new JLabel();
         whatsapp.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/whatsapp-icon.png")));
-        whatsapp.setBounds(width / 30, width / 2 - 15, 500, 100);
+        whatsapp.setBounds(47, 705, 26, 26);
         frame.getContentPane().add(whatsapp);
 
         whatsapp = new JLabel("(94) 99129-0504");
-        whatsapp.setBounds(125, width / 2 - 25, 500, 100);
-        whatsapp.setFont(new Font("Helvetica Neue", Font.PLAIN, 22));
+        whatsapp.setBounds(88, 698, 140, 19);
+        whatsapp.setFont(new Font("Helvetica Neue", Font.PLAIN, 18));
         whatsapp.setForeground(new Color(255, 255, 255));
         frame.getContentPane().add(whatsapp);
 
         whatsapp = new JLabel("(94) 98146-2634");
-        whatsapp.setBounds(125, width / 2 + 3, 500, 100);
-        whatsapp.setFont(new Font("Helvetica Neue", Font.PLAIN, 22));
+        whatsapp.setBounds(88, 718 , 140, 19);
+        whatsapp.setFont(new Font("Helvetica Neue", Font.PLAIN, 18));
         whatsapp.setForeground(new Color(255, 255, 255));
         frame.getContentPane().add(whatsapp);
 
         instagram = new JLabel("   Barbearia.Navalhas");
         instagram.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/instagram-icon.png")));
-        instagram.setBounds(width / 3 + 120, width / 2 - 15, 500, 100);
-        instagram.setFont(new Font("Helvetica Neue", Font.PLAIN, 22));
+        instagram.setBounds(496, 705, 220, 27);
+        instagram.setFont(new Font("Helvetica Neue", Font.PLAIN, 18));
         instagram.setForeground(new Color(255, 255, 255));
         frame.getContentPane().add(instagram);
 
         facebook = new JLabel("   @barbearianavalhasparauapebas");
         facebook.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/facebook-icon.png")));
-        facebook.setBounds(width - 450, width / 2 - 15, 500, 100);
-        facebook.setFont(new Font("Helvetica Neue", Font.PLAIN, 22));
+        facebook.setBounds(1000, 705, 340, 26);
+        facebook.setFont(new Font("Helvetica Neue", Font.PLAIN, 18));
         facebook.setForeground(new Color(255, 255, 255));
         frame.getContentPane().add(facebook);
 
         rodape = new JPanel();
-        rodape.setBounds(0, width / 2, width, width / 24 + 50);
+        rodape.setBounds(0, 688, 1366, 80);
         rodape.setBackground(new Color(34, 34, 34));
         rodape.setLayout(null);
         frame.getContentPane().add(rodape);
